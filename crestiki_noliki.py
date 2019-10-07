ece5656bfe329f9a9808bdf635e122a088dbc129
RocknRolla <RocknRolla>
2019-10-07 20:42:10 +0300

    Flake8 fix v.1

--- crestiki_noliki.py
+++ crestiki_noliki.py
@@ -2,38 +2,38 @@ class Game:
 
     def __init__(self):
         self.field = [
-        "   |   |   ",
-        "   |   |   ",
-        "___|___|___",
-        "   |   |   ",
-        "   |   |   ",
-        "___|___|___",
-        "   |   |   ",
-        "   |   |   ",
-        "   |   |   "
+            "   |   |   ",
+            "   |   |   ",
+            "___|___|___",
+            "   |   |   ",
+            "   |   |   ",
+            "___|___|___",
+            "   |   |   ",
+            "   |   |   ",
+            "   |   |   "
         ]
 
         self.map = {
-        7: (7, 1),
-        8: (7, 5),
-        9: (7, 9),
-        4: (4, 1),
-        5: (4, 5),
-        6: (4, 9),
-        1: (1, 1),
-        2: (1, 5),
-        3: (1, 9)
+            7: (7, 1),
+            8: (7, 5),
+            9: (7, 9),
+            4: (4, 1),
+            5: (4, 5),
+            6: (4, 9),
+            1: (1, 1),
+            2: (1, 5),
+            3: (1, 9)
         }
 
         self.win_combinations = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 6, 9],
-        [1, 5, 9],
-        [3, 5, 7]
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7]
         ]
 
         self.crestiki = []
@@ -50,8 +50,8 @@ class Game:
             self.noliki.append(cell)
         row = self.map[cell][0]
         column = self.map[cell][1]
-        self.field[row] = self.field[row][:column] + sign.capitalize() + self.field[row][column + 1:]
-
+        self.field[row] = self.field[row][:column] +\
+            sign.capitalize() + self.field[row][column + 1:]
 
     def check_win_condition(self):
         for combination in self.win_combinations:
